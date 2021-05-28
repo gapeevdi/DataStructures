11c6daaac6284b8e75a4d317b5b316d743a8123d
Dmitry <gapeevdirog@gmail.com>
2021-05-28 16:31:32 +0300

    added Add_AlternatingOrder_TreeHeightLog2Count

--- Tests/AvlTreeTests.cs
+++ Tests/AvlTreeTests.cs
@@ -8,6 +8,10 @@ namespace Tests
     public class AVLTreeTests
     {
 
+        private static bool IsExpectedTreeHeight(int nodeCount, int actualHeight) =>
+            actualHeight >= Math.Floor(Math.Log2(nodeCount))
+            && actualHeight <= Math.Floor(1.45 * Math.Log2(nodeCount + 2));
+        
         private static int[] GenerateArray(int startWith, int count = 1, int step = 1)
         {
             var array = new int[count];
@@ -21,7 +25,7 @@ namespace Tests
             return array;
         }
 
-        private static int FillTree(AVLTree<int> tree, int startWith, int count = 1, int step = 1)
+        private static int FillTreeInLinearOrder(AVLTree<int> tree, int startWith, int count = 1, int step = 1)
         {
             var valuesToAdd = GenerateArray(startWith, count, step);
             var addedValuesCount = 0;
@@ -33,6 +37,25 @@ namespace Tests
 
             return addedValuesCount;
         }
+
+        private static int FillTreeInAlternateOrder(AVLTree<int> tree, int startRange, int endRange, int step = 1)
+        {
+            var lowValue = startRange;
+            var topValue = endRange;
+            var count = 0;
+
+            while (topValue > lowValue)
+            {
+                tree.Add(lowValue);
+                tree.Add(topValue);
+                count += 2;
+
+                lowValue += step;
+                topValue -= step;
+            }
+
+            return count;
+        }
         
         [TestCase(0, 1, 10)]
         [TestCase(-1, 2, 10)]
@@ -43,13 +66,14 @@ namespace Tests
         [TestCase(-500, 1000, 10)]
         [TestCase(-5000, 10000, 10)]
         [TestCase(-50000, 100000, 10)]
+        [TestCase(-500000, 1000001, 10)]
         public void Add_EachItemGreaterThanPrevious_TreeHeightLog2Count(int startWith, int count, int step)
         {
             var tree = new AVLTree<int>();
-            var addedValuesCount = FillTree(tree, startWith, count, step);
+            var addedValuesCount = FillTreeInLinearOrder(tree, startWith, count, step);
             
             Assert.AreEqual(count, tree.Count);
-            Assert.AreEqual(Math.Floor(Math.Log2(addedValuesCount)), tree.Height);
+            Assert.IsTrue(IsExpectedTreeHeight(addedValuesCount, tree.Height));
         }
 
         [TestCase(0, 1, -10)]
@@ -61,13 +85,30 @@ namespace Tests
         [TestCase(-500, 1000, -10)]
         [TestCase(-5000, 10000, -10)]
         [TestCase(-50000, 100000, -10)]
+        [TestCase(-500000, 1000000, -10)]
         public void Add_EachItemLessThanPrevious_TreeHeightLog2Count(int startWith, int count, int step)
         {
             var tree = new AVLTree<int>();
-            var addedValuesCount = FillTree(tree, startWith, count, step);
+            var addedValuesCount = FillTreeInLinearOrder(tree, startWith, count, step);
             
             Assert.AreEqual(count, tree.Count);
-            Assert.AreEqual(Math.Floor(Math.Log2(addedValuesCount)), tree.Height);
+            Assert.IsTrue(IsExpectedTreeHeight(addedValuesCount, tree.Height));
+        }
+
+        [TestCase(-10, 10, 1)]
+        [TestCase(-100, 100, 5)]
+        [TestCase(-1000, 1000, 10)]
+        [TestCase(-10000, 10000, 3)]
+        [TestCase(-10000, 10000, 3)]
+        [TestCase(-50000, 50000, 7)]
+        [TestCase(-500000, 500000, 11)]
+        public void Add_AlternatingOrder_TreeHeightLog2Count(int start, int end, int step)
+        {
+            var tree = new AVLTree<int>();
+            var addedValuesCount = FillTreeInAlternateOrder(tree, start, end, step);
+            
+            Assert.AreEqual(addedValuesCount, tree.Count);
+            Assert.IsTrue(IsExpectedTreeHeight(addedValuesCount, tree.Height));
         }
 
         [Test]
@@ -83,7 +124,7 @@ namespace Tests
             tree.Add(50);
 
             var height = tree.Height;
-            Assert.AreEqual(tree.Height, Math.Floor(Math.Log2(tree.Count)));
+            Assert.AreEqual(tree.Height, Math.Ceiling(Math.Log2(tree.Count)));
         }
         
         [Test]
@@ -99,7 +140,7 @@ namespace Tests
             tree.Add(150);
 
             var height = tree.Height;
-            Assert.AreEqual(tree.Height, Math.Floor(Math.Log2(tree.Count)));
+            Assert.AreEqual(tree.Height, Math.Ceiling(Math.Log2(tree.Count)));
         }
 
         [Test]
